b250830c9c30a4e731ae095d74f67ef2f0063831
Chet Hill <chetdhill@gmail.com>
2025-09-15 17:52:37 -0500

    Add Bar Chart for Sales by Channel Chart

--- src/components/overview/CategoryDistributionChart.jsx
+++ src/components/overview/CategoryDistributionChart.jsx
@@ -9,7 +9,7 @@ const categoryData = [
   { name: "Tour", value: 1900 }, 
 ];
 
-const COLORS = ["#6366F1", "#8B5CF6", "#EC4899", "#10B981", "#F59E0B"]
+const COLORS = ["#6366F1", "#8B5CF6", "#EC4899", "#10B981", "#F59E0B"];
 
 const CategoryDistributionChart = () => {
   return (
