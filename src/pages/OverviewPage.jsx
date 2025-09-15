6e3df56b11ebd79c6834fa46e3db26230f789a7e
Chet Hill <chetdhill@gmail.com>
2025-09-15 12:59:18 -0500

    Create StatCard Component

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,9 +1,14 @@
 import Header from "../components/common/Header";
+import { motion } from "framer-motion";
 
 const OverviewPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Overview" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STATS */}
+        <motion.div></motion.div>
+      </main>
     </div>
   )
 }
