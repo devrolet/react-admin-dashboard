6d9b6826a22d7ad5196348902534342a9710a707
Chet Hill <chetdhill@gmail.com>
2025-09-15 12:53:25 -0500

    Create Header Component

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,6 +1,10 @@
+import Header from "../components/common/Header";
+
 const OverviewPage = () => {
   return (
-    <div>OverviewPage</div>
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+    </div>
   )
 }
 
