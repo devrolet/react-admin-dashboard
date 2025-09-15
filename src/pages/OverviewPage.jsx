ca0d6fc8b0761fa2df5bc3c2dc29a9efb5068643
Chet Hill <chetdhill@gmail.com>
2025-09-15 16:27:05 -0500

    Create StatCard component with sample data and labels

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,5 +1,7 @@
 import Header from "../components/common/Header";
 import { motion } from "framer-motion";
+import StatCard from "../components/common/StatCard";
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
 
 const OverviewPage = () => {
   return (
@@ -7,7 +9,26 @@ const OverviewPage = () => {
       <Header title="Overview" />
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         {/* STATS */}
-        <motion.div></motion.div>
+        <motion.div 
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y:20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          {/* TODO: Turn this into a data-mapped component */}
+          <StatCard 
+            name="Total Sales" icon={Zap} value="$12,345" color="#6366F1"
+          />
+          <StatCard 
+            name="New Users" icon={Users} value="1,234" color="#8B5CF6"
+          />
+          <StatCard 
+            name="Total Products" icon={ShoppingBag} value="459" color="#EC4899"
+          />
+          <StatCard 
+            name="Conversion Rate" icon={BarChart2} value="78%" color="#10B981"
+          />
+        </motion.div>
       </main>
     </div>
   )
