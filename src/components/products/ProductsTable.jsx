cc7af8ef47dd1499a023dcf16edb9b8970a81fe4
Chet Hill <chetdhill@gmail.com>
2025-09-16 08:53:30 -0500

    Quick Syntax Edit for readability

--- src/components/products/ProductsTable.jsx
+++ src/components/products/ProductsTable.jsx
@@ -49,34 +49,22 @@ const ProductTable = () => {
         >
           <thead>
             <tr>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Name
               </th>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Category
               </th>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Price
               </th>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Stock
               </th>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Sales
               </th>
-              <th
-                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-              >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Actions
               </th>
             </tr>
