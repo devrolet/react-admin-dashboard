2001adfd9fc20d61d9cca68e35cabcbd9c41885e
Chet Hill <chetdhill@gmail.com>
2025-09-15 19:37:42 -0500

    Create SalesTrendChart component and Products Page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,43 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { AlertTriangle, DollarSign, Package, TrendingUp } from "lucide-react";
+
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import ProductTable from "../components/products/ProductTable";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesTrendChart from "../components/products/SalesTrendChart";
+
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STATS */}
+        <motion.div 
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y:20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          {/* TODO: Turn this into a data-mapped component */}
+          <StatCard name="Total Products" icon={Package} value="1234" color="#6366F1" />
+          <StatCard name="Top Selling" icon={TrendingUp} value="98" color="#10B981" />
+          <StatCard name="Low Stock" icon={AlertTriangle} value="23" color="#F59e0B" />
+          <StatCard name="Total Revenue" icon={DollarSign} value="$543,219" color="#ef4444" />
+        </motion.div>
+
+        <ProductTable />
+
+        {/* CHARTS */}
+        <div
+          className="grid grid-col-1 lg:grid-cols-2 gap-8"
+        >
+          <SalesTrendChart />
+          <CategoryDistributionChart />
+        </div>
+      </main>
+    </div>
   )
 }
 
