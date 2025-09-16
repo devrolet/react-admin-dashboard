113b3d12dfda57864c48ce2bcdcddcf81881ca22
Chet Hill <chetdhill@gmail.com>
2025-09-16 08:52:55 -0500

    Finish UserTable.jsx

--- src/components/users/UsersTable.jsx
+++ src/components/users/UsersTable.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import { Search } from "lucide-react";
+import { Search, Edit, Trash2 } from "lucide-react";
 import { useState } from "react";
 
 const UsersTable = () => {
@@ -67,6 +67,7 @@ const UsersTable = () => {
                   <div className="flex items-center">
                     <div className="flex-shrink-0 h-10 w-10">
                       <div className="h-10 w-10 rounded-full bg-gradient-to-r from-purple-400 to-blue-500 flex items-center justify-center text-white font-semibold">
+                        {/* TODO: Create Function to display user image here if avail */}
                         {user.name.charAt(0)}
                       </div>
                     </div>
@@ -74,6 +75,39 @@ const UsersTable = () => {
                       <div className="text-sm font-medium text-gray-100">{user.name}</div>
                     </div>
                   </div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-300">{user.email}</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-800 text-blue-100">
+                    {user.role}
+                  </span>
+                </td>
+
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                    user.status === "Active"
+                    ? "bg-green-800 text-green-100"
+                    : "bg-red-800 text-red-100"
+                  }`}>
+                    {user.status}
+                  </span>
+                </td>
+
+                  {/* TEXT EDIT/DELETE CODE: SAVE JIC */}
+                {/* <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  <button className="text-indigo-400 hover:text-indigo-300 mr-2">Edit</button>
+                  <button className="text-red-400 hover:text-red-300">Delete</button>
+                </td> */}
+
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300 mr-2">
+                  <button className="text-indigo-400 hover:text-indigo-300 mr-2">
+                    <Edit size={18} />
+                  </button>
+                   <button className="text-red-400 hover:text-indigo-300 mr-2">
+                    <Trash2 size={18} />
+                  </button>
                 </td>
               </motion.tr>
             ))}
