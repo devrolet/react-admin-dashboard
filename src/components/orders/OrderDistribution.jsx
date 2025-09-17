2a6d4db48da3d9c8f79561e22b14283940058b81
Chet Hill <chetdhill@gmail.com>
2025-09-17 09:17:49 -0500

    Complete OrderDistribution Chart functionality

--- src/components/orders/OrderDistribution.jsx
+++ src/components/orders/OrderDistribution.jsx
@@ -1,6 +1,54 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts";
+
+const COLORS = ["#b6e2dd", "#e9e5af", "#fbc99d", "#fbb39d", "#fba09d"];
+
+// TODO: BE SURE TO USE name INSTEAD OF NAME
+const ORDER_STATUS_DATA = [
+  { name: "Pending", value: 30 },
+  { name: "Processing", value: 45 },
+  { name: "Shipped", value: 60 },
+  { name: "Delivered", value: 120 }, 
+];
+
 const OrderDistribution = () => {
   return (
-    <div>OrderDistribution</div>
+    <motion.div
+          className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}
+        >
+          <h2 className="text-lg font-medium mb-4 text-gray-100">Order Status Distribution</h2>
+          <div style={{ width: "100%", height: 300 }}>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie
+                  data={ORDER_STATUS_DATA}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={100}
+                  fill="#8884d8"
+                  dataKey={"value"}
+                  label={({name, percent}) => `${name} ${(percent * 100).toFixed(0)}%`}
+                >
+                  {ORDER_STATUS_DATA.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+                <Tooltip 
+                  contentStyle={{
+                    backgroundColor: "rgba(31, 41, 55, 0.8)",
+                    borderColor: "#4B5563"
+                  }}
+                  itemStyle={{ color: "#E5E7EB"}}
+                />
+                <Legend />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+        </motion.div>
   )
 }
 
